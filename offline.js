da45f95f4e04cee622810f90a988e1f57b009d72
Davi Arcos Baptista <30696945+baptixta@users.noreply.github.com>
2018-10-05 13:24:37 -0300

    Game
    Jogo finalizado.

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1522202652,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/fundo.png",
+		"images/fundo2.png",
+		"images/serra.png",
+		"images/jogador-sheet0.png",
+		"images/plataformas-sheet0.png",
+		"images/titulo-sheet0.png",
+		"images/ruby-sheet0.png",
+		"images/ruby-sheet1.png",
+		"media/press start 8bit gamer effects  sound effects (hd).m4a",
+		"media/press start 8bit gamer effects  sound effects (hd).ogg",
+		"media/what is love 8 bit.m4a",
+		"media/what is love 8 bit.ogg",
+		"media/grito.m4a",
+		"media/grito.ogg",
+		"media/sfx_jump_50.m4a",
+		"media/sfx_jump_50.ogg",
+		"media/coin 8bit gamer effects  sound effects (hd).m4a",
+		"media/coin 8bit gamer effects  sound effects (hd).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
